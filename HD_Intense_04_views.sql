f12743117c43a00d61aa0945247a9ba41b7b141c
Aleksander Kupiec <olek@sip.local>
2020-05-11 12:50:28 +0200

    hd_faktura_poz_view - przywórcenie brakujących pól oraz poprawnych nazw

--- HD_Intense_04_views.sql
+++ HD_Intense_04_views.sql
@@ -186,14 +186,15 @@ OSP_KorektaDoOspOrgKey
 ,decode(p.lp_dod,0,'',trim(to_char(p.nr_komp_doks,'0000000000'))||trim(to_char(p.nr_poz,'00000'))||trim(to_char(0,'00')))OSP_GlownaDoOspOrgKey
    ,decode(p.lp_dod,0,'',p.indeks) OSP_NazwaDod
    ,decode(p.lp_dod,0,'',p.ilosc) OSP_IloscPol
-   ,decode(p.lp_dod,0,'',p.netto_wal) OSP_CenaEwidencyjnaPola
+   --,decode(p.lp_dod,0,'',p.netto_wal) OSP_CenaEwidencyjnaPolaPLN
+   ,0 OSP_CenaEwidencyjnaPolaPLN
    ,0 OSP_CenaEwidencyjnaPLN
    ,0 OSP_CenaEwidencyjnam2PLN
    ,0 OSP_CenaNettoPLN
    ,0 OSP_CenaNettoPoRabaciePLN
    ,0 OSP_CenaBruttoPLN
    ,0 OSP_CenaBruttoPoRabaciePLN
-   ,decode(p.lp_dod,0,'',p.cena_netto) OSP_CenaEwidencyjnaPolaPLN
+   ,decode(p.lp_dod,0,'',p.cena_netto) OSP_CenaEwidencyjnaPola
    ,p.netto_b_wal OSP_WartoscPrzedRabatem
    ,p.netto_wal OSP_WartoscPoRabacie
    ,p.wart_netto_b OSP_WartoscPrzedRabatemPLN
@@ -203,7 +204,8 @@ OSP_KorektaDoOspOrgKey
    ,p.cena_netto_szt OSP_CenaNettoSztuki
    ,p.nr_poz OSP_NrPoz
    ,decode(p.rodzaj_ceny,'z³/s','szt','m2') OSP_RodzajCeny
-   ,pd.cena_surowcowa/pd.ilosc OSP_CenaSurowcowa
+   ,decode(p2.ilosc,0,pd.cena_surowcowa,pd.cena_surowcowa/p2.ilosc) OSP_CenaSurowcowaM2
+   ,decode(p2.il_szt,0,pd.cena_surowcowa,pd.cena_surowcowa/p2.il_szt) OSP_CenaSurowcowaSzt
 from &&CUTTER_SCHEMA..fakpoz p
 left join &&CUTTER_SCHEMA..faknagl f on f.nr_komp=p.nr_komp_doks 
 left join pozdok dp on dp.nr_komp_dok=p.id_wz and dp.nr_poz=p.id_wz_poz left join dok d on d.NR_KOMP_DOK=dp.nr_dok_zrod 
@@ -212,6 +214,9 @@ left join (
     left join dok d on d.nr_komp_dok=pd.nr_komp_dok
     where pd.typ_dok='WZ' group by d.nr_kom_fakt,pd.nr_komp_baz,pd.nr_poz_zlec
 ) pd on pd.nr_kom_fakt=p.nr_komp_doks and pd.nr_komp_baz=p.id_zlec and pd.nr_poz_zlec=p.id_zlec_poz
+left join (
+    select fp.nr_komp_doks,fp.id_zlec,fp.id_zlec_poz,sum(ilosc) ilosc,sum(il_szt) il_szt from fakpoz fp where czy_dod<>'T' group by fp.nr_komp_doks,fp.id_zlec,fp.id_zlec_poz
+) p2 on p2.nr_komp_doks=p.nr_komp_doks and p2.id_zlec=p.id_zlec and p2.id_zlec_poz=p.id_zlec_poz;
 where f.data_wyst between to_date('01-04-2020','DD-MM-YYYY') and to_date('31-03-2021','DD-MM-YYYY')
 WITH READ ONLY;
 /
